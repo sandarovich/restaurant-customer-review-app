e951b17a5f1c75017076bf910ac9cb05f885dcc9
sandarovich <sandarovich@ukr.net>
2020-03-05 00:07:29 +0200

    add basic dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 ; Web
+                 [ring "1.8.0"]
+                 [compojure "1.6.1"]
+
+                 ; Database
+                 [toucan "1.1.9"]
+                 [org.postgresql/postgresql "42.2.4"]
+
+                 ]
   :main ^:skip-aot restaurant-customer-review-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
