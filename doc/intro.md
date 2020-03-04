d2639b810cd18227168bff0ba33aa0df4fe991a6
sandarovich <sandarovich@ukr.net>
2020-03-04 22:51:30 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to restaurant-customer-review-app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/restaurant_customer_review_app/core_test.clj
@@ -0,0 +1,7 @@
+(ns restaurant-customer-review-app.core-test
+  (:require [clojure.test :refer :all]
+            [restaurant-customer-review-app.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
