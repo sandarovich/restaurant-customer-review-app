2f2f2c46af5bfed122bdc588fcfff48650978bfd
sandarovich <sandarovich@ukr.net>
2020-03-05 20:37:18 +0200

    add persistence layer (part 1)

--- test/restaurant_customer_review_app/core_test.clj
+++ test/restaurant_customer_review_app/core_test.clj
@@ -1,6 +1,6 @@
 (ns restaurant-customer-review-app.core-test
   (:require [clojure.test :refer :all]
-            [restaurant-customer-review-app.core :refer :all]))
+            [restaurant-customer-review-app.main.core :refer :all]))
 
 (deftest a-test
   (testing "FIXED. Not failing any more"
